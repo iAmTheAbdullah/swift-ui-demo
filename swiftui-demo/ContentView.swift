a6cd2fe55cb48187d4b9568ed67d57eba447cbd1
Abdullah Malik <7953568+iAmTheAbdullah@users.noreply.github.com>
2020-05-10 17:03:31 +0100

    Initial Commit

---
+++ swiftui-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-demo
+//
+//  Created by Abdullah Malik on 10/05/2020.
+//  Copyright © 2020 Abdullah Malik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
